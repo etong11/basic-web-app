385609d26322aeeecfcadb54b2fd4d73af7d2d8a
Emma Tong <53305901+etong11@users.noreply.github.com>
2024-10-10 09:59:41 -0400

    Added max numbers + plus query

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -16,6 +16,27 @@ export default function QueryProcessor(query: string): string {
       "Emma"
     );
   }
+  // "Which of the following numbers is the largest: 1, 2, 3, 4"
+  else if (query.toLowerCase().includes("Which of the following numbers is the largest:")) {
+    let numbers = query.split(":")[1].split(",");
+    // Trim space at the beginning
+    numbers = numbers.map((num) => num.trim());
+    let max = Number.MIN_SAFE_INTEGER;
+    for (let i = 0; i < numbers.length; i++) {
+      const num = parseInt(numbers[i]);
+      if (num > max) {
+        max = num;
+      }
+    }
+    return max.toString();
+  }
+  // What is 33 plus 80?
+  else if (query.toLowerCase().includes("plus")) {
+    let numbers = query.split("plus");
+    const num1 = parseInt(numbers[0].split("is")[1].trim());
+    const num2 = parseInt(numbers[1].trim());
+    return (num1 + num2).toString();
+  }
 
   return "";
 }
