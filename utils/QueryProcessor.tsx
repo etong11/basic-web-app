742f8d4daf0536406362af2b024d376cb2448f43
Emma Tong <53305901+etong11@users.noreply.github.com>
2024-10-10 09:47:33 -0400

    Added name query

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -11,6 +11,11 @@ export default function QueryProcessor(query: string): string {
       "eltong"
     );
   }
+  else if (query.toLowerCase().includes("name")) {
+    return (
+      "Emma"
+    );
+  }
 
   return "";
 }
